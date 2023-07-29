ac5e0ddf5f0c3dc5dfbcf8fba89f1245360d310a
ScignScape-RZ <osrworkshops@gmail.com>
2023-07-29 13:01:28 -0400

    update

--- code/cpp/src/prelim/contrib/nathaniel/cutelyst/-build_/run-cmake/run-cmake.sh
+++ code/cpp/src/prelim/contrib/nathaniel/cutelyst/-build_/run-cmake/run-cmake.sh
@@ -1,13 +1,22 @@
-cmake=`cat ../preferred/which/cmake`
+cmake=`cat ../../preferred/which/cmake`
 echo "Using cmake: $cmake"
+
+prefix=`cat ../../preferred/which/cmake-prefix-path`
+
+re='(.*)\\\s+(.*)'
+while [[ $prefix =~ $re ]]; do
+  prefix=${BASH_REMATCH[1]}${BASH_REMATCH[2]}
+done
+
+echo "Using prefix-path: $prefix"
 $cmake \
  -D CMAKE_BUILD_TYPE=Debug \
- -D CMAKE_C_COMPILER=`cat ../preferred/which/gcc` \
- -D CMAKE_CXX_COMPILER=`cat ../preferred/which/g++` \
- -D CMAKE_PREFIX_PATH=`cat ../preferred/which/cmake-prefix-path` \
+ -D CMAKE_C_COMPILER=`cat ../../preferred/which/cc` \
+ -D CMAKE_CXX_COMPILER=`cat ../../preferred/which/cxx` \
+ -D CMAKE_PREFIX_PATH="$prefix" \
  -D CMAKE_INSTALL_PREFIX=../../install \
  -D PLUGIN_VIEW_GRANTLEE:BOOL=ON \
- ../../../cutelyst
+ ../../../framework
 
 # -D CMAKE_PREFIX_PATH=/quasihome/nlevisrael/opencv/xnfinstall/lib/cmake/opencv4/ \
 # PLUGIN_VIEW_GRANTLEE:BOOL=OFF
