7fdf7ff1bdbb9e7aa0e41d8a252c2bf317476637
ScignScape-RZ <osrworkshops@gmail.com>
2023-07-29 23:04:47 -0400

    update

--- code/cpp/src/prelim/contrib/nathaniel/cutelyst/apps/ello/tsi/run-cutelyst.sh
+++
@@ -1,9 +0,0 @@
-# shell script to launch cutelyst generators ...
-# note: the arguments after the cutelyst invocation will pass the -h
-# argument unless this script is called with one or more alternative parameters
-#
-cd ../ello;
-LD_LIBRARY_PATH=/home/nlevisrael/gits/dcr-wip/ar/code/cpp/src/prelim/contrib/nathaniel/cutelyst/-build_/install/lib:\
-/home/nlevisrael/Qt/5.15.2/gcc_64/lib:$LD_LIBRARY_PATH \
-/home/nlevisrael/gits/dcr-wip/ar/code/cpp/src/prelim/contrib/nathaniel/cutelyst/-build_/install/bin/cutelyst3-qt5 ${1:-h} ${@:2:$#} ;
-cd --
